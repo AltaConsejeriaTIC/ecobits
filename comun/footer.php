740fced6ec2e6e2c077efc169d75f7d5f722867a
LuisCastilloB <lacb93@gmail.com>
2014-10-10 03:11:11 -0500

    [dev] Corrige footer

--- comun/footer.php
+++ comun/footer.php
@@ -1,43 +1,12 @@
-    <div class="navbar-wrapper" id="menu_principal" style="opacity:0">
-      <div class="container">
-      <div class="col-lg-2" >
-      <img src="img/logo.png" id="logo">
-      </div>
-      <div class="col-lg-10">
-        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
-          <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              
-            </div>
-            <div class="navbar-collapse collapse">
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="http://getbootstrap.com/examples/carousel/#">
-                  Inicio
-                </a></li>
-                <li><a href="http://getbootstrap.com/examples/carousel/#about">Sobre Nosotros</a></li>
-                <li><a href="http://getbootstrap.com/examples/carousel/#contact">Comentarios</a></li>
-                <li class="dropdown">
-                  <a href="http://getbootstrap.com/examples/carousel/#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-                  <ul class="dropdown-menu" role="menu">
-                    <li><a href="http://getbootstrap.com/examples/carousel/#">Action</a></li>
-                    <li><a href="http://getbootstrap.com/examples/carousel/#">Another action</a></li>
-                    <li><a href="http://getbootstrap.com/examples/carousel/#">Something else here</a></li>
-                    <li class="divider"></li>
-                    <li class="dropdown-header">Nav header</li>
-                    <li><a href="http://getbootstrap.com/examples/carousel/#">Separated link</a></li>
-                    <li><a href="http://getbootstrap.com/examples/carousel/#">One more separated link</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        </div>
-      </div>
-    </div>
+<footer>
+  <div class="col-xs-1">
+  git
+  </div>
+  <div class="col-xs-1">
+  fb
+  </div>
+  <div class="col-xs-1">
+  tw
+  </div>
+
+</footer>
