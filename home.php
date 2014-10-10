e36d832cb1c97ef6e9cd7f2096363fb7d28e2c62
LuisCastilloB <lacb93@gmail.com>
2014-10-10 11:55:43 -0500

    [dev] MOdificaicon del home

--- home.php
+++ home.php
@@ -54,7 +54,6 @@ require_once "comun/footer.php";
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.js"></script>
-    <script src="js/bootstrap.min.js"></script>
     <script src="js/menu_reduccion.js"></script>
     
 
