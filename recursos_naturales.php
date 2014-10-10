5f0dc024e640a88603ec36ba9de583fdf8456ac0
JoseLineros <jose_2907@hotmail.com>
2014-10-10 11:35:56 -0500

    [dev] modificacion recursos naturales

--- recursos_naturales.php
+++ recursos_naturales.php
@@ -8,7 +8,7 @@ if(isset($_SESSION['id'])){
 <html lang="es">
 <head>
   <meta charset="UTF-8">
-  <title>Index2</title>
+  <title>Recursos Naturales</title>
     <link href="style/css/bootstrap.css" rel="stylesheet">
     <link href="style/css/carrusel.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="style/css/general.css">
@@ -35,17 +35,27 @@ cuerpo también sea agua. Quizás sea por eso que lo recomendable para tener una
 y una larga vida sea el comer alimentos con un porcentaje del 70 por ciento en agua.
 <hr><br>
 
+<!-- Large modal -->
+<button class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg">Large modal</button>
 
-<!-- Button trigger modal -->
-<button type="button" data-toggle="modal" data-target="#myModal">Launch modal</button>
-
-<div class="modal fade bs-example-modal-sm" id="myModal" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-sm">
+<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
-      ...
+       <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
     </div>
   </div>
 </div>
+
 
   </div>
   <div class="col-xs-3"><b>El Aire</b>
@@ -96,10 +106,9 @@ require_once "comun/footer.php";
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.js"></script>
-    <script src="js/bootstrap.min.js"></script>
     <script src="js/menu_reduccion.js"></script>
     <script type="text/javascript" >
-    //$('#myModal').modal('toggle');
+    $('#myModal').modal('toggle');
         </script>
     
 
