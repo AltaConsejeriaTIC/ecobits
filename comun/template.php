1ec8d875ed1566fffc355f1f0fada018758110de
JoseLineros <jose_2907@hotmail.com>
2014-10-10 02:41:44 -0500

    [dev] Modificando el template

---
+++ comun/template.php
@@ -0,0 +1,74 @@
+    <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="item">
+          <img src="img/city.png" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Example headline.</h1>
+              <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Gy rules.</p>
+            </div>
+          </div>
+        </div>
+        <div class="item active">
+          <img src="img/bar.png" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Another example headline.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="img/bar.png" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>One more for good measure.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="http://getbootstrap.com/examples/carousel/#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+      <a class="right carousel-control" href="http://getbootstrap.com/examples/carousel/#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+    </div>
+
+
+<!--menu principal-->
+<div  id="menu_secundario">
+  <div class="container">
+ <div class="row placeholders">
+      <div class="col-xs-4 col-sm-2 placeholder _menu2 m1">
+        <img class="img-responsive"  src="img/logo.png">
+        <div>Something else</div>
+      </div>
+      <div class="col-xs-4 col-sm-2 placeholder _menu2 m2">
+       <img class="img-responsive "  src="img/logo.png">
+        <div class="">Something else</div>
+      </div>
+      <div class="col-xs-4 col-sm-2 placeholder _menu2 m3">
+         <img class="img-responsive" src="img/logo.png">
+        <div class="">Something else</div>
+      </div>
+      <div class="col-xs-4 col-sm-2 placeholder _menu2 m4">
+          <img class="img-responsive "  src="img/logo.png">
+        <div class="">Something else</div>
+      </div>
+       <div class="col-xs-4 col-sm-2 placeholder _menu2 m5">
+         <img class="img-responsive "  src="img/logo.png">
+        <div class="">Something else</div>
+      </div>
+       <div class="col-xs-4 col-sm-2 placeholder _menu2 m6">
+        <img class="img-responsive "  src="img/logo.png">
+        <div class="">Something else</div>
+      </div>
+    </div>
+  </div>
+</div>
