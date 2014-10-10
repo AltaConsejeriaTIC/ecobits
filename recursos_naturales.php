615e814e8e93b33547337530003375c7bdfb190f
JoseLineros <jose_2907@hotmail.com>
2014-10-10 12:00:31 -0500

    [dev] modificacion recursos_naturales

--- recursos_naturales.php
+++ recursos_naturales.php
@@ -120,14 +120,13 @@ require_once "comun/footer.php";
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>   
+    <script src="js/jquery.js"></script> 
     <script src="js/bootstrap.js"></script>
 
 
     <script src="js/menu_reduccion.js"></script>
     <script type="text/javascript" >
-    $('#myModal').modal('toggle');
+   
         </script>
   
 </body>
