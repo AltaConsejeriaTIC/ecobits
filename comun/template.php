37e0e6d751a824afa9a73333967205dddfac3d36
JoseLineros <jose_2907@hotmail.com>
2014-10-10 05:18:38 -0500

    [dev] Modificando template

--- comun/template.php
+++ comun/template.php
@@ -6,6 +6,7 @@
         <li data-target="#myCarousel" data-slide-to="0" class=""></li>
         <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
         <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="3" class=""></li>
       </ol>
       <div class="carousel-inner">
         <div class="item">
@@ -18,16 +19,16 @@
           </div>
         </div>
         <div class="item active">
-          <img src="img/banner1.png" alt="Second slide">
+          <img src="img/banner2.jpg" alt="Second slide">
           <div class="container">
             <div class="carousel-caption">
-              <h1>Another example headline.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <h1>Cuida la naturaleza</h1>
+              <p>cuida de la naturaleza ya que este nos ofrece muy buenos recursos</p>
             </div>
           </div>
         </div>
         <div class="item">
-          <img src="img/banner2.png" alt="Third slide">
+          <img src="img/banner1.png" alt="Third slide">
           <div class="container">
             <div class="carousel-caption">
               <h1>One more for good measure.</h1>
@@ -35,6 +36,17 @@
             </div>
           </div>
         </div>
+
+        <div class="item">
+          <img src="img/banner4.jpg" alt="Four slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>One more for good measure.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </div>
+          </div>
+        </div>
+
       </div>
       <a class="left carousel-control" href="http://getbootstrap.com/examples/carousel/#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
       <a class="right carousel-control" href="http://getbootstrap.com/examples/carousel/#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
@@ -50,8 +62,8 @@
         <div>Recursos Naturales</div>
       </div>
       <div class="col-xs-4 col-sm-2 placeholder _menu2 m2">
-       <img class="img-responsive "  src="img/cam.png">
-        <div class=""></div>
+       <img class="img-responsive "  src="img/clima.png">
+        <div class="">Cambio Climatico</div>
       </div>
       <div class="col-xs-4 col-sm-2 placeholder _menu2 m3">
          <img class="img-responsive" src="img/salud.png">
@@ -62,8 +74,8 @@
         <div class="">Reciclaje</div>
       </div>
        <div class="col-xs-4 col-sm-2 placeholder _menu2 m5">
-         <img class="img-responsive "  src="img/clima.png">
-        <div class="">Cambio Climatico</div>
+         <img class="img-responsive "  src="img/cam.png">
+        <div class=""></div>
       </div>
        <div class="col-xs-4 col-sm-2 placeholder _menu2 m6">
         <img class="img-responsive "  src="img/book.png">
