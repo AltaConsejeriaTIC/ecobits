0d56136a40733a4e37d4698cf62489dbcf07745c
LuisCastilloB <lacb93@gmail.com>
2014-10-10 14:34:30 -0500

    [dev] para jose luis

--- comun/footer.php
+++ comun/footer.php
@@ -1,12 +1,19 @@
+
+<script type="text/javascript" src="js/social.js"></script>
 <footer>
-  <div class="col-xs-1">
-  git
-  </div>
-  <div class="col-xs-1">
-  fb
-  </div>
-  <div class="col-xs-1">
-  tw
+  <div class="col-xs-12">
+	<div id="redes_sociales">
+	    <table class="redes_sociales" align="center">
+	        <tr>
+	            <td><a href="localhost/ecobits" target="_blank" title="Ver el proyecto en GitHub"><img src="img/github.png" style="width:30px"></a></td>
+
+	            <td><div class="fb-like" data-href="localhost/ecobits/eco/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div></td>
+
+	            <td><a href="https://twitter.com/share" class="twitter-share-button" data-url="localhos/ecobits" data-text="Aprede e informate en EcoBits, Bogotá lo necesita" data-hashtags="HackathonPorLaPaz">Tweet</a>
+	            </td>
+	        </tr>
+	    </table>
+	</div>
   </div>
 
 </footer>
