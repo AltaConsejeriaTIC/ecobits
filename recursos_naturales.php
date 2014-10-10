60dc9833c8e9767697a013940dcd6a7b3b6bc6c3
JoseLineros <jose_2907@hotmail.com>
2014-10-10 08:19:13 -0500

    [dev] modificacion de archivo recursos naturales

--- recursos_naturales.php
+++ recursos_naturales.php
@@ -1,10 +1,27 @@
-<<!DOCTYPE html>
-<html lang="en">
+<?php
+require_once 'comun/class.dbDriver.php';
+$db = new DBDriver(PDOConfig::getInstance());
+session_start();
+if(isset($_SESSION['id'])){
+?>
+<!DOCTYPE html>
+<html lang="es">
 <head>
   <meta charset="UTF-8">
-  <title>RECURSOS NATURALES</title>
+  <title>Index2</title>
+    <link href="style/css/bootstrap.css" rel="stylesheet">
+    <link href="style/css/carrusel.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style/css/general.css">
+    
 </head>
 <body>
+
+  <?php
+  require_once "comun/menu.php";
+  require_once "comun/template.php";
+  ?>
+
+<!--contenido pagina-->
 
 <div class="container" data-contenido="page" id="contenido">
 <article>
@@ -50,6 +67,30 @@ formación, el mismo se forma en gran parte gracias a la misma vegetación y la 
 
 </article>
 </div>
+
+<!--fin contenido pagina-->
+
+<?php
+require_once "comun/footer.php";
+?>
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/menu_reduccion.js"></script>
+    
+
   
 </body>
 </html>
+<?php
+}else{
+  header('Location: index.html');
+}
+
+?>
